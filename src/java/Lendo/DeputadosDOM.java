70398bf8787f5f4fd414b7f3ce6a6e90b88ebf9e
Lucas <lucasdejesus000@gmail.com>
2018-11-20 10:54:40 -0200

    Lendo arquivo xml de deputados

--- src/java/Lendo/DeputadosDOM.java
+++ src/java/Lendo/DeputadosDOM.java
@@ -23,7 +23,7 @@ import org.xml.sax.SAXException;
  * @author Lucas
  */
 public class DeputadosDOM {
-     public void fazerParsing(String pathArq) throws SAXException,
+    public void fazerParsing(String pathArq) throws SAXException,
             IOException, ParserConfigurationException {
 
         File xmlFile = new File(pathArq);
@@ -110,7 +110,7 @@ public class DeputadosDOM {
                 deputado.setFone(fone);
                 deputado.setEmail(email);
                 
-                dao.addDeputado(deputado);
+                //dao.addDeputado(deputado);
                 System.out.println(deputado.toString());
                 
             } // end if
