16ebcb85a9f34da587eec436edb6620f57add52a
Allan Duque <43408655+allanduque@users.noreply.github.com>
2018-11-20 10:35:56 -0200

    Adicionei a tabela partido, e fiz a alteração no jsp.

---
+++ src/java/Consultas/TesteQueriesPartidos.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Consultas;
+
+import Modelos.Deputados;
+import Modelos.Partidos;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Allan
+ */
+public class TesteQueriesPartidos {
+    /**
+     * @param args the command line arguments
+     */
+    
+    public List<Partidos> buscaPartidos(){
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("Deputados");
+        EntityManager manager = factory.createEntityManager();
+
+        Query query = manager.createQuery("SELECT e FROM Partidos e");
+        List partidos = query.getResultList();
+        
+        return partidos;
+        
+    }
+    
+}
